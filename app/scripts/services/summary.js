bffff4ac487b5223b4c334a80fbc7bf260ce8503
José Abarca Saavedra <jabarca@trabajando.com>
2015-11-23 15:18:18 -0300

    arreglos

--- app/scripts/services/summary.js
+++ app/scripts/services/summary.js
@@ -23,34 +23,35 @@ angular.module('moodFrontendApp')
       }
 
       function yesterday(cb){
-          var total = 0;
-          var sum = [
-            {
-                moodid: 1,
-                total: 2
-            },
-            {
-                moodid: 4,
-                total: 3
-            },
-            {
-                moodid: 2,
-                total: 5
-            }
-        ];
-        //cantidad de respuestas
-        for(var mod in sum) {
-            total +=  sum[mod].total;
-        }
+          var url = 'http://chile-mobile.demotbj.com/moods/yesterday';
+          var req = $http.get(url);
+          req.then(function(result){
+              console.log('result', result);
+              var sum = result.data;
+              sum = order(sum);
+              cb(sum);
+          }, function(err){
+              console.log('error:', err)
+          })
+      }
 
-        //porcentaje
-        if (total > 0) {
-            for(var mod in sum) {
-                sum[mod].percent = parseInt((sum[mod].total/total) * 100);
-            }
-        }
-        sum.sort(compare);
-        cb(sum);
+      function order(sum){
+          var total = 0;
+          //cantidad de respuestas
+          for(var mod in sum) {
+              if (sum[mod].total) {
+                total +=  sum[mod].total;
+              }
+          }
+
+          //porcentaje
+          if (total > 0) {
+              for(var mod in sum) {
+                  sum[mod].percent = parseInt((sum[mod].total/total) * 100);
+              }
+          }
+          sum.sort(compare);
+          return sum;
       }
 
       //Ordena de mayor a menor
